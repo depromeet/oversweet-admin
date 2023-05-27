c87be830dce572cccd448fe5f74c6db391da0bff
JeJune Lee <81547780+LeeJejune@users.noreply.github.com>
2023-05-27 22:50:06 +0900

    test: 프랜차이즈 이미지 변경 테스트 코드 작성

--- admin-api/src/test/java/com/depromeet/franchise/service/FranchiseEntityServiceTest.java
+++ admin-api/src/test/java/com/depromeet/franchise/service/FranchiseEntityServiceTest.java
@@ -2,25 +2,23 @@ package com.depromeet.franchise.service;
 
 
 import com.depromeet.common.exception.franchise.FranchiseAlreadyExistException;
+import com.depromeet.common.exception.franchise.FranchiseImageUrlDuplicateException;
 import com.depromeet.domain.franchise.FranchiseRepository;
 import com.depromeet.domain.franchise.domain.Franchise;
-import com.depromeet.domain.franchise.entity.FranchiseEntity;
 import com.depromeet.franchise.dto.request.CreateFranchiseRequest;
+import com.depromeet.franchise.dto.request.ModifyFranchiseImageRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.exceptions.misusing.FriendlyReminderException;
 import org.mockito.junit.jupiter.MockitoSettings;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.never;
@@ -40,7 +38,7 @@ class FranchiseEntityServiceTest {
     @DisplayName("프랜차이즈 - 성공")
     void saveFranchise() {
         // given
-        CreateFranchiseRequest request = franchiseRequest();
+        CreateFranchiseRequest request = createFranchiseRequest();
         Franchise franchise = franchise(1L);
         Mockito.doNothing().when(franchiseRepository).save(Mockito.any(Franchise.class));
 
@@ -57,7 +55,7 @@ class FranchiseEntityServiceTest {
     @Test
     @DisplayName("프랜차이즈 - 중복으로 인한 실패")
     void saveFranchiseFailedWhenDuplicateFranchiseName() {
-        CreateFranchiseRequest request = franchiseRequest();
+        CreateFranchiseRequest request = createFranchiseRequest();
         Franchise testFranchise = toDomain(request);
 
         given(franchiseRepository.findByName(request.name())).willReturn(Optional.of(testFranchise));
@@ -70,6 +68,57 @@ class FranchiseEntityServiceTest {
                 .findByName(request.name());
     }
 
+    @Test
+    @DisplayName("프랜차이즈 이미지 Url 수정 - 성공")
+    void modifyFranchiseImageUrl() {
+        // given
+        ModifyFranchiseImageRequest request = modifyFranchiseImageUrlRequest();
+        Franchise franchise = franchise(1L);
+
+        given(franchiseRepository.findById(1L)).willReturn((franchise));
+
+        // when
+        franchiseService.modifyFranchiseImage(1L, request);
+
+        // then
+        assertThat(franchise.getImageUrl()).isEqualTo(request.imageUrl());
+
+        then(franchiseRepository)
+                .should(times(1))
+                .updateImage(any());
+    }
+
+    @Test
+    @DisplayName("프랜차이즈 이미지 Url 수정 - 실패 (이미 같은 이미지 url)")
+    void modifyFranchiseImageUrlWhenUrlIsForbidden() {
+        // given
+        ModifyFranchiseImageRequest request = modifyFranchiseImageUrlBadRequest();
+        Franchise franchise = franchise(1L);
+
+        given(franchiseRepository.findById(1L)).willReturn((franchise));
+
+        // when
+        assertThatThrownBy(() -> franchiseService.modifyFranchiseImage(1L, request))
+                .isInstanceOf(FranchiseImageUrlDuplicateException.class);
+
+        // then
+        assertThat(franchise.getImageUrl()).isEqualTo(request.imageUrl());
+
+        then(franchiseRepository)
+                .should(never())
+                .updateImage(any());
+    }
+
+    private ModifyFranchiseImageRequest modifyFranchiseImageUrlRequest() {
+        return ModifyFranchiseImageRequest.builder()
+                .imageUrl("test1")
+                .build();
+    }
+    private ModifyFranchiseImageRequest modifyFranchiseImageUrlBadRequest() {
+        return ModifyFranchiseImageRequest.builder()
+                .imageUrl("test2")
+                .build();
+    }
 
     private Franchise toDomain(CreateFranchiseRequest request) {
         return Franchise.builder()
@@ -78,7 +127,7 @@ class FranchiseEntityServiceTest {
                 .build();
     }
 
-    private CreateFranchiseRequest franchiseRequest() {
+    private CreateFranchiseRequest createFranchiseRequest() {
         return CreateFranchiseRequest.builder()
                 .name("test")
                 .imageUrl("test")
