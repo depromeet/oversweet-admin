10f30791d98cada4ac34a8077511197042b78c9b
JeJune Lee <81547780+LeeJejune@users.noreply.github.com>
2023-05-27 19:27:19 +0900

    fix: 안쓰는 로직 제거

--- admin-core/src/main/java/com/depromeet/domain/franchise/entity/FranchiseEntityRepository.java
+++ admin-core/src/main/java/com/depromeet/domain/franchise/entity/FranchiseEntityRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface FranchiseEntityRepository extends JpaRepository<FranchiseEntity, Long> {
-    Optional<FranchiseEntity> findByName(String name);
 }
