c40a2ecd608c3f169e71f6e36eeaaab0aa091d5e
JeJune Lee <81547780+LeeJejune@users.noreply.github.com>
2023-06-06 14:27:17 +0900

    [Fix] Error Response 처리 (#29)

--- admin-api/src/main/java/com/depromeet/franchise/controller/FranchiseController.java
+++ admin-api/src/main/java/com/depromeet/franchise/controller/FranchiseController.java
@@ -1,6 +1,7 @@
 package com.depromeet.franchise.controller;
 
 
+import com.depromeet.common.exception.ErrorResponse;
 import com.depromeet.common.response.ApiCommonResponse;
 import com.depromeet.common.response.ApiMessageResponse;
 import com.depromeet.franchise.dto.request.CreateFranchiseRequest;
@@ -8,6 +9,8 @@ import com.depromeet.franchise.dto.request.ModifyFranchiseImageRequest;
 import com.depromeet.franchise.dto.response.FranchiseResponse;
 import com.depromeet.franchise.service.FranchiseService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
@@ -33,7 +36,7 @@ public class FranchiseController {
     @Operation(summary = "프랜차이즈 생성", description = "프랜차이즈 생성 API입니다.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "성공적으로 프랜차이즈가 생성되었습니다."),
-            @ApiResponse(responseCode = "409", description = "이미 존재하는 프랜차이즈입니다.")
+            @ApiResponse(responseCode = "409", description = "이미 존재하는 프랜차이즈입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @PostMapping
     public ResponseEntity<ApiMessageResponse> franchiseSave(@RequestBody final CreateFranchiseRequest request) {
@@ -45,8 +48,8 @@ public class FranchiseController {
     @Operation(summary = "프랜차이즈 이미지 변경", description = "프랜차이즈 이미지 수정 API입니다.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "성공적으로 프랜차이즈가 생성되었습니다."),
-            @ApiResponse(responseCode = "400", description = "존재 하지 않는 프랜차이즈입니다."),
-            @ApiResponse(responseCode = "409", description = "같은 이미지 입니다.")
+            @ApiResponse(responseCode = "400", description = "존재 하지 않는 프랜차이즈입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "409", description = "같은 이미지 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @PatchMapping("/{id}")
     public ResponseEntity<ApiMessageResponse> franchiseModifyImage(@PathVariable(name = "id") final Long id,
