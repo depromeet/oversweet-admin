f82fdee482608643a5eab0f9a3b30feea091f78e
JeJune Lee <81547780+LeeJejune@users.noreply.github.com>
2023-07-04 10:13:09 +0900

    feature: 음료 삭제 API 구현

--- admin-api/src/main/java/com/depromeet/common/DrinkApiResponseDescription.java
+++ admin-api/src/main/java/com/depromeet/common/DrinkApiResponseDescription.java
@@ -5,6 +5,7 @@ public final class DrinkApiResponseDescription {
     public static final String SUCCESS_DRINKS_FETCHED = "성공적으로 음료 목록이 조회되었습니다.";
     public static final String SUCCESS_DRINK_IMAGE_URL_MODIFIED = "성공적으로 음료의 이미지가 수정 되었습니다.";
     public static final String SUCCESS_DRINK_MODIFIED = "성공적으로 음료 데이터 수정이 되었습니다.";
+    public static final String SUCCESS_DRINK_DELETE = "성공저으로 해당 음료를 삭제했습니다.";
     public static final String NOT_FOUND_DRINK = "존재 하지 않는 음료입니다.";
     public static final String NOT_ALLOWED_DRINK_IMAGE_URL = "잘못된 음료 이미지 Url입니다.";
 
